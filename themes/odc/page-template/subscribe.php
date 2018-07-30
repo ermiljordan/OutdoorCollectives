c3a865850bb0d880d117df9cfa069c95243e8a32
chiwwy <ermiljordan64@gmail.com>
2018-07-30 11:34:15 +0900

    connect to local host

--- themes/odc/page-template/subscribe.php
+++ themes/odc/page-template/subscribe.php
@@ -15,7 +15,11 @@ get_header();
         <div class="sub-text container">
           <h2 class="italic">First<span class="bold">look</span></h2>
           <h3 class="sub-cap">Become an expert camper!</h3>
-          <p class="sub-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas finibus sodales urna nec congue. Nullam id eros mattis quam hendrerit ultrices eu ut neque. Praesent id molestie nisl. Integer commodo porta massa in tempus. Fusce hendrerit volutpat leo at dignissim. Pellentesque et auctor odio. In posuere laoreet arcu eleifend finibus.</p>
+          <p class="sub-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+             Maecenas finibus sodales urna nec congue. Nullam id eros mattis quam hendrerit 
+             ultrices eu ut neque. Praesent id molestie nisl. Integer commodo porta massa in tempus.
+              Fusce hendrerit volutpat leo at dignissim. Pellentesque et auctor odio. In posuere laoreet 
+              arcu eleifend finibus.</p>
           <?php echo do_shortcode('[mc4wp_form id="38"]'); ?> 
         </div>
       </section>
